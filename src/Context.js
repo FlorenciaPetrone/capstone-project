025afd3cd41ca824eb097cc45c7331e46dd4985a
Florencia Petrone <florenciapetrone@Florencias-MacBook-Pro.local>
2021-04-30 17:04:39 +0200

    changes-in-fetch

--- src/Context.js
+++ src/Context.js
@@ -1,80 +1,84 @@
-import React, {useState, useEffect} from "react"
-import { createClient } from 'pexels';
+import React, { useState, useEffect } from "react";
+import { createClient } from "pexels";
 
 //Photos API setup
-const Context = React.createContext()
-const client = createClient('563492ad6f9170000100000141700883e61b45c7a0df7c6679957214');
+const Context = React.createContext();
+const client = createClient(
+  "563492ad6f9170000100000141700883e61b45c7a0df7c6679957214"
+);
 
-function ContextProvider({children}){
-    //State
-    const [allPhotos, setAllPhotos] = useState([])
-    const [cartItems, setCartItems] = useState([])
-    const [favoritedPhotos, setFavoritedPhotos] = useState([])
-    //API related
-    const [config, setConfig] = useState({
-        query: "Nature",
-        per_page: 40,
-        page: 1
-    })
-    //Pagination
-    const [paginationRelated, setPaginationRelated] = useState({
-        perPage: 40,
-        page: 1,
-        totalResults: 10
-    })
+function ContextProvider({ children }) {
+  //State
+  const [allPhotos, setAllPhotos] = useState([]);
+  const [cartItems, setCartItems] = useState([]);
+  const [favoritedPhotos, setFavoritedPhotos] = useState([]);
+  //API related
+  const [config, setConfig] = useState({
+    query: "Nature",
+    per_page: 80,
+    page: 1,
+  });
+  //Pagination
+  const [paginationRelated, setPaginationRelated] = useState({
+    perPage: 80,
+    page: 1,
+    totalResults: 10,
+  });
 
-    useEffect(() => {
-        client.photos.search(config).then((res) => {
-          setAllPhotos(res.photos);
-          const { page, per_page, total_results } = res;
-          setPaginationRelated({
-            currentPage: page,
-            perPage: per_page,
-            totalResults: total_results,
-          });
-        });
-      }, [config]);
+  useEffect(() => {
+    client.photos.search(config).then((res) => {
+      setAllPhotos(res.photos);
+      const { page, per_page, total_results } = res;
+      setPaginationRelated({
+        currentPage: page,
+        perPage: per_page,
+        totalResults: total_results,
+      });
+    });
+  }, [config]);
 
-    //Favorite related
-    function toggleFavorite(id) {
-        const updatedArr = allPhotos.map(photo => {
-            if(photo.id === id){
-                return {...photo, isFavorite: !photo.isFavorite}
-            }
-            return photo
-        })
-        setAllPhotos(updatedArr)
-        const favoritedArr = updatedArr.filter(photo => 
-            photo.isFavorite === true     
-        )
-        setFavoritedPhotos(favoritedArr)
-    }
-    
-    //Cart related 
-    function addImage(newItem){
-        setCartItems(prevItem => [...prevItem, newItem])
-    }
+  //Favorite related
+  function toggleFavorite(id) {
+    const updatedArr = allPhotos.map((photo) => {
+      if (photo.id === id) {
+        return { ...photo, isFavorite: !photo.isFavorite };
+      }
+      return photo;
+    });
+    setAllPhotos(updatedArr);
+    const favoritedArr = updatedArr.filter(
+      (photo) => photo.isFavorite === true
+    );
+    setFavoritedPhotos(favoritedArr);
+  }
 
-    function removeFromCart(id){
-        setCartItems(prevItems => prevItems.filter(item => item.id !== id) )
-    }
+  //Cart related
+  function addImage(newItem) {
+    setCartItems((prevItem) => [...prevItem, newItem]);
+  }
 
-    return(
-        <Context.Provider value={{
-            allPhotos, 
-            cartItems,
-            addImage, 
-            removeFromCart,  
-            setCartItems,
-            toggleFavorite,  
-            favoritedPhotos,
-            requestConfig: config,
-            setRequestConfig: setConfig,
-            paginationRelated,
-        }}>
-            {children}
-        </Context.Provider>
-    )
+  function removeFromCart(id) {
+    setCartItems((prevItems) => prevItems.filter((item) => item.id !== id));
+  }
+
+  return (
+    <Context.Provider
+      value={{
+        allPhotos,
+        cartItems,
+        addImage,
+        removeFromCart,
+        setCartItems,
+        toggleFavorite,
+        favoritedPhotos,
+        requestConfig: config,
+        setRequestConfig: setConfig,
+        paginationRelated,
+      }}
+    >
+      {children}
+    </Context.Provider>
+  );
 }
- 
-export  {ContextProvider, Context}
+
+export { ContextProvider, Context };
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,34 +1,33 @@
 //State and Context
-import React, { useState, useContext } from "react"
-import { Context } from "../Context"
-
+import React, { useState, useContext } from "react";
+import { Context } from "../Context";
 
 const Search = () => {
-    const [userInput, setUserInput] = useState("")
-    const { requestConfig, setRequestConfig } = useContext(Context)
+  const [userInput, setUserInput] = useState("");
+  const { requestConfig, setRequestConfig } = useContext(Context);
 
+  return (
+    <div className="search-container">
+      <input
+        type="text"
+        placeholder="Write a theme"
+        onChange={(e) => setUserInput(e.target.value)}
+      />
+      <button
+        className="button-hover"
+        disabled={!userInput.length}
+        onClick={() =>
+          setRequestConfig({
+            query: userInput,
+            page: 1,
+            per_page: 80,
+          })
+        }
+      >
+        Search
+      </button>
+    </div>
+  );
+};
 
-    return(
-        <div className="search-container">
-            <input 
-                type="text" 
-                placeholder="Write a theme"
-                onChange={e => setUserInput(e.target.value)}
-            />
-            <button
-                className="button-hover"
-                disabled={!userInput.length}
-                onClick={() => 
-                    setRequestConfig({
-                        query: userInput,
-                        page: 1,
-                        per_page: 40,
-                    })
-                }
-            
-            >Search</button> 
-        </div>
-    )
-}
-
-export default Search
+export default Search;
